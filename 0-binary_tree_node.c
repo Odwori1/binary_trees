904c41a4a8a9ca554efec0a0754126e7b616bf83
Odwori1 <odwori.okuku@gmail.com>
2024-02-28 22:57:06 -0800

    correct create

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary node
+ * binary_tree_node - Create a binary node
  * @parent: The parent node
- * @value: value of the node
- * Return: newnode
+ * @value: Value of the node
+ * Return: new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	if (!new_node)
+		return (NULL);
 
-    return (new_node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
